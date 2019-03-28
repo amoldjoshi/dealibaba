9cfc91ee10d19aa5033622a359474354668333c9
Amol <amoldjoshi@gmail.com>
2019-03-28 01:18:58 -0700

    before aws s3

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,3 +1,3 @@
 class Dealer < ApplicationRecord
-	has_one_attached :image
+	has_one_attached :logo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :products
   resources :dealers
   devise_for :users
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
