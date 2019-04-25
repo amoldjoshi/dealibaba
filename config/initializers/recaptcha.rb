f0b4322343ad04234210815e6dd93a64bbc4c533
Amol <amoldjoshi@gmail.com>
2019-04-24 20:48:33 -0700

    recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,6 @@
+
 Recaptcha.configure do |config|
-  	config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
-  	config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
+  config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  config.use_ssl_by_default = true
 end
