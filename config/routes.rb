1b925aef36c683d5a71708078cccef232bfaeb57
Amol <amoldjoshi@gmail.com>
2019-04-28 15:32:02 -0700

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,11 @@ Rails.application.routes.draw do
   get '/finders', to: "dealers#finders"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+    Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        registrations: "registrations"
+      }
+    end
+
 
 end
