75671fc725a1a90e71d57754f13e2713794d658e
Amol <amoldjoshi@gmail.com>
2019-04-29 23:16:07 -0700

    recaptchadevise

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,16 @@ Rails.application.routes.draw do
 
 
   #devise_for :users, controllers:{registrations: "registrations"}
-  devise_for :users, controllers: { registrations: "registrations"}
-
+  
   get '/innovations', to: "products#innovations"
   get '/weeklyadds', to: "dealers#weeklyadds"
   get '/dailydeals', to: "dealers#dailydeals"
   get '/finders', to: "dealers#finders"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    Rails.application.routes.draw do
+      devise_for :users
+    end
+
 
 end
