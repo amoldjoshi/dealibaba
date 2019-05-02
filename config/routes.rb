a7518350b4424e6ba7ff2007c68a1d34a4ecb7d0
Amol <amoldjoshi@gmail.com>
2019-05-02 07:04:28 -0700

    added new registration

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,18 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
 
-  devise_for :users, controllers:{registrations: "registrations"}
+  #devise_for :users, controllers:{registrations: "registrations"}
   
   get '/innovations', to: "products#innovations"
   get '/weeklyadds', to: "dealers#weeklyadds"
   get '/dailydeals', to: "dealers#dailydeals"
   get '/finders', to: "dealers#finders"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    Rails.application.routes.draw do
+      devise_for :users, controllers:{registrations: "registrations"}
+
+    end
 
 
 end
