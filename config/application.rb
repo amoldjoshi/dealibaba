0193cf1a52c02242ea39105b66c65997f5cfd9a7
Amol <amoldjoshi@gmail.com>
2019-05-02 07:01:31 -0700

    added new registration

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
+#Amol added following line for recaptcha
+require 'net/http' 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
