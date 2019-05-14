72fd1bafb2a141dd89e68762a6e39cc3793aa86f
Amol <amoldjoshi@gmail.com>
2019-05-14 00:00:28 -0700

    Changed root

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   get '/finders', to: "dealers#finders"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '*path' => redirect('/')
+
 end
