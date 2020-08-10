4b5d6fc9420becad3124ce130efcc298027702a4
Amol <amoldjoshi@gmail.com>
2020-08-10 14:43:29 -0700

    added animation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -81,6 +81,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:productid, :productname, :producturl, :producttype, :productdescription, :productresearch, :productprice, :productbutton, :productvisible, :picture, :productcategory)
+      params.require(:product).permit(:productid, :productname, :producturl, :producttype, :productdescription, :productresearch, :productprice, :productbutton, :productvisible, :picture, :productcategory, :animation)
     end
 end
